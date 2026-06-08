6cfb40512a93abc71751f16ac2c1c35977ce793e
燕资伟 <>
2026-06-08 17:40:03 +0800

    Await ky create test request

--- tests/routes/things/create.test.ts
+++ tests/routes/things/create.test.ts
@@ -1,15 +1,19 @@
+import { expect, test } from "bun:test"
 import { getTestServer } from "tests/fixtures/get-test-server"
-import { test, expect } from "bun:test"
 
 test("create a thing", async () => {
   const { ky } = await getTestServer()
 
-  ky.post("things/create", {
-    json: {
-      name: "Thing1",
-      description: "Thing1 Description",
-    },
-  })
+  const createResponse = await ky
+    .post("things/create", {
+      json: {
+        name: "Thing1",
+        description: "Thing1 Description",
+      },
+    })
+    .json<{ ok: boolean }>()
+
+  expect(createResponse).toEqual({ ok: true })
 
   const data = await ky
     .get("things/list")
