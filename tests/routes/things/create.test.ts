1ec84a1bad9f3813e44ce33c82bf0c3b1f97681e
Sai Teja  Bandaru <Saitejaroyal2311@gmail.com>
2026-06-12 19:02:35 +0200

    test: await ky post and add delete route test

--- tests/routes/things/create.test.ts
+++ tests/routes/things/create.test.ts
@@ -4,12 +4,16 @@ import { test, expect } from "bun:test"
 test("create a thing", async () => {
   const { ky } = await getTestServer()
 
-  ky.post("things/create", {
-    json: {
-      name: "Thing1",
-      description: "Thing1 Description",
-    },
-  })
+  const res = await ky
+    .post("things/create", {
+      json: {
+        name: "Thing1",
+        description: "Thing1 Description",
+      },
+    })
+    .json<{ ok: boolean }>()
+
+  expect(res.ok).toBe(true)
 
   const data = await ky
     .get("things/list")
