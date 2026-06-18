bd0c669031e465c4d4bdeaa5fbaf55239ad774d6
xgcing <894987633@qq.com>
2026-06-18 11:49:57 +0800

    test: await ky create route response

--- tests/routes/things/create.test.ts
+++ tests/routes/things/create.test.ts
@@ -3,17 +3,24 @@ import { test, expect } from "bun:test"
 
 test("create a thing", async () => {
   const { ky } = await getTestServer()
+  const thing = {
+    name: "Thing1",
+    description: "Thing1 Description",
+  }
 
-  ky.post("things/create", {
-    json: {
-      name: "Thing1",
-      description: "Thing1 Description",
-    },
-  })
+  const createData = await ky
+    .post("things/create", {
+      json: thing,
+    })
+    .json<{ ok: boolean }>()
+
+  expect(createData).toEqual({ ok: true })
 
   const data = await ky
     .get("things/list")
     .json<{ things: { name: string; description: string }[] }>()
 
   expect(data.things).toHaveLength(1)
+  expect(data.things[0]?.name).toBe(thing.name)
+  expect(data.things[0]?.description).toBe(thing.description)
 })
